78235e8e34bacddc3b9fc61f2bd421eb1ed2e926
kaue <kauezinho_naruto@hotmail.com>
2021-11-07 15:39:39 -0300

    RecyclerLayout_Image

--- app/src/main/java/com/example/affirmations2/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations2/model/Affirmation.kt
@@ -1,5 +1,8 @@
 package com.example.affirmations2.model
 
-data class Affirmation(val stringResourceId: Int) {
-
-}
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
